0fce4fa26de3a9f24cb67dea6ea1a13732950896
QYing <ying.qixin@gmail.com>
2017-09-22 15:02:05 +0200

    success in serving html in browser

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Vous êtes à l\'accueil');
+ 	res.render('index.ejs');
 });
+
 
 app.listen(8080);
