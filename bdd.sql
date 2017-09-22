af6cf7376655a201c7085c7b505075d8c70a5ead
Elliezar Rayray <elliezar.rayray@gmail.com>
2017-09-22 14:21:49 +0200

    Add file of data base,Table User will be created and some users will be implemented

---
+++ bdd.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Utilisateur {
+ 	IdUser int NOT NULL AUTO_INCREMENT,
+	Username VARCHAR 100,
+	Password VARCHAR 100,
+	Email VARCHAR 100
+};
+
+INSERT INTO Utilisateur VALUES ('anthony','anthony','anthony@barei.fr');
+INSERT INTO Utilisateur VALUES ('elliezar','elliezar','elliezar@rayray.fr');
+INSERT INTO Utilisateur VALUES ('qixin','qixin','qixin@ying.fr');
